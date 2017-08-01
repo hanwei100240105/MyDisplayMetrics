1fd064a5bd809437ffef6d55655b2e4dc9512ddb
hanwei100240105 <13520231305@163.com>
2017-08-01 21:56:07 +0800

    [update]对齐

--- app/src/main/java/bupt/com/badgeview/MainActivity.java
+++ app/src/main/java/bupt/com/badgeview/MainActivity.java
@@ -14,23 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bt=(Button)findViewById(R.id.bt);
-         BadgeView badgeView = new BadgeView(this);
+        Button bt = (Button) findViewById(R.id.bt);
+        BadgeView badgeView = new BadgeView(this);
         badgeView.setTargetView(findViewById(R.id.rl));
         badgeView.setBadgeGravity(Gravity.RIGHT | Gravity.CENTER);
         badgeView.setBadgeMargin(0, 0, 10, 0);
         badgeView.setText("韩维");
         final BadgeView badgeView2 = new BadgeView(this);
         badgeView2.setTargetView(findViewById(R.id.rl2));
-bt.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {//点击按钮出现随机数
-        Random r=new Random();
-        int randNum = r.nextInt(1000);//0-1000的随机数
-        badgeView2.setBadgeCount(randNum);
-        //
-    }
-});
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {//点击按钮出现随机数
+                Random r = new Random();
+                int randNum = r.nextInt(1000);//0-1000的随机数
+                badgeView2.setBadgeCount(randNum);
+
+            }
+        });
 
 
     }
