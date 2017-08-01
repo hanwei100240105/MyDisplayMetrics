0a5f157c3dd41be328c856a5f51dace0700ae78d
hanwei100240105 <13520231305@163.com>
2017-08-01 20:36:04 +0800

    [update]

---
+++ app/src/main/java/bupt/com/badgeview/MainActivity.java
@@ -0,0 +1,36 @@
+package bupt.com.badgeview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt=(Button)findViewById(R.id.bt);
+         BadgeView badgeView = new BadgeView(this);
+        badgeView.setTargetView(findViewById(R.id.rl));
+        badgeView.setBadgeGravity(Gravity.RIGHT | Gravity.CENTER);
+        badgeView.setBadgeMargin(0, 0, 10, 0);
+        badgeView.setText("韩维");
+        final BadgeView badgeView2 = new BadgeView(this);
+        badgeView2.setTargetView(findViewById(R.id.rl2));
+bt.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {//点击按钮出现随机数
+        Random r=new Random();
+        int randNum = r.nextInt(1000);//0-1000的随机数
+        badgeView2.setBadgeCount(randNum);
+    }
+});
+
+
+    }
+}
